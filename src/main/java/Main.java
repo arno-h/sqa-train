1eeea3273ee8070bc0da6b9a5ffe205c908799e1
Arno Hollosi <ahollosi@xmp.net>
2023-03-31 17:29:17 +0200

    fix some code inspection warnings; give loop variable in calcDecade() a better name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,17 @@
 import java.io.*;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         Main main = new Main();
-
-        File csv = new File(Main.class.getResource("BritishTrainFatalities.csv").toURI());
+        URL pathURL = Main.class.getResource("BritishTrainFatalities.csv");
+        if (pathURL == null) {
+            System.out.println("Couldn't load CSV file");
+            return;
+        }
+        File csv = new File(pathURL.toURI());
         FileInputStream csvStream = new FileInputStream(csv);
         PrintWriter out = new PrintWriter(System.out, true);
         main.stats(csvStream, out);
@@ -26,20 +31,19 @@ public class Main {
         Integer road_fatal_sum = 0;
         Integer move_acc_sum = 0;
         Integer move_fatal_sum = 0;
-        Integer old_decade = -1;
+        int old_decade = -1;
         for (int i = 0; i < trainData.decade.size(); i++) {
             Integer new_decade = trainData.decade.get(i);
             if (new_decade != old_decade) {
                 if (old_decade != -1) {
-                    out.println(String.format(
-                            "Decade %d: " + "rides_mkm=%d, coll_acc=%d, " +
+                    out.printf("Decade %d: " + "rides_mkm=%d, coll_acc=%d, " +
                                     "coll_fatal=%d, road_acc=%d, " +
                                     "road_fatal=%d, move_acc=%d, " +
-                                    "move_fatal=%d",
+                                    "move_fatal=%d\n",
                             1900 + old_decade * 10, rides_mkm_sum,
                             coll_acc_sum, coll_fatal_sum,
                             road_acc_sum, road_fatal_sum,
-                            move_acc_sum, move_fatal_sum));
+                            move_acc_sum, move_fatal_sum);
                 }
                 old_decade = new_decade;
                 rides_mkm_sum = trainData.rides_mkm.get(i);
@@ -59,15 +63,14 @@ public class Main {
                 move_fatal_sum += trainData.move_fatal.get(i);
             }
         }
-        out.println(String.format(
-                "Decade %d: " + "rides_mkm=%d, coll_acc=%d, " +
+        out.printf("Decade %d: " + "rides_mkm=%d, coll_acc=%d, " +
                         "coll_fatal=%d, road_acc=%d, " +
                         "road_fatal=%d, move_acc=%d, " +
-                        "move_fatal=%d",
+                        "move_fatal=%d\n",
                 1900 + old_decade * 10, rides_mkm_sum,
                 coll_acc_sum, coll_fatal_sum,
                 road_acc_sum, road_fatal_sum,
-                move_acc_sum, move_fatal_sum));
+                move_acc_sum, move_fatal_sum);
 
         // average per decade
         out.println("\nAverage per decade ====================");
@@ -84,15 +87,14 @@ public class Main {
             Integer new_decade = trainData.decade.get(i);
             if (new_decade != old_decade) {
                 if (old_decade != -1) {
-                    out.println(String.format(
-                            "Decade %d: " + "rides_mkm=%.2f, coll_acc=%.2f, " +
+                    out.printf("Decade %d: " + "rides_mkm=%.2f, coll_acc=%.2f, " +
                                     "coll_fatal=%.2f, road_acc=%.2f, " +
                                     "road_fatal=%.2f, move_acc=%.2f, " +
-                                    "move_fatal=%.2f",
+                                    "move_fatal=%.2f\n",
                             1900 + old_decade * 10, (double) rides_mkm_avg / decade_cnt,
                             (double) coll_acc_avg / decade_cnt, (double) coll_fatal_avg / decade_cnt,
                             (double) road_acc_avg / decade_cnt, (double) road_fatal_avg / decade_cnt,
-                            (double) move_acc_avg / decade_cnt, (double) move_fatal_avg / decade_cnt));
+                            (double) move_acc_avg / decade_cnt, (double) move_fatal_avg / decade_cnt);
                 }
                 old_decade = new_decade;
                 rides_mkm_avg = trainData.rides_mkm.get(i);
@@ -114,14 +116,13 @@ public class Main {
                 decade_cnt++;
             }
         }
-        out.println(String.format(
-                "Decade %d: " + "rides_mkm=%.2f, coll_acc=%.2f, " +
+        out.printf("Decade %d: " + "rides_mkm=%.2f, coll_acc=%.2f, " +
                         "coll_fatal=%.2f, road_acc=%.2f, " +
                         "road_fatal=%.2f, move_acc=%.2f, " +
-                        "move_fatal=%.2f",
+                        "move_fatal=%.2f\n",
                 1900 + old_decade * 10, ((double) rides_mkm_avg) / decade_cnt,
                 ((double) coll_acc_avg) / decade_cnt, ((double) coll_fatal_avg) / decade_cnt,
                 ((double) road_acc_avg) / decade_cnt, ((double) road_fatal_avg) / decade_cnt,
-                ((double) move_acc_avg) / decade_cnt, ((double) move_fatal_avg) / decade_cnt));
+                ((double) move_acc_avg) / decade_cnt, ((double) move_fatal_avg) / decade_cnt);
     }
 }
--- src/main/java/TrainData.java
+++ src/main/java/TrainData.java
@@ -3,15 +3,15 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TrainData {
-    ArrayList<Integer> year = new ArrayList<>();
-    ArrayList<Integer> rides_mkm = new ArrayList<>();
-    ArrayList<Integer> coll_acc = new ArrayList<>();
-    ArrayList<Integer> coll_fatal = new ArrayList<>();
-    ArrayList<Integer> road_acc = new ArrayList<>();
-    ArrayList<Integer> road_fatal = new ArrayList<>();
-    ArrayList<Integer> move_acc = new ArrayList<>();
-    ArrayList<Integer> move_fatal = new ArrayList<>();
-    ArrayList<Integer> decade = new ArrayList<>();
+    final ArrayList<Integer> year = new ArrayList<>();
+    final ArrayList<Integer> rides_mkm = new ArrayList<>();
+    final ArrayList<Integer> coll_acc = new ArrayList<>();
+    final ArrayList<Integer> coll_fatal = new ArrayList<>();
+    final ArrayList<Integer> road_acc = new ArrayList<>();
+    final ArrayList<Integer> road_fatal = new ArrayList<>();
+    final ArrayList<Integer> move_acc = new ArrayList<>();
+    final ArrayList<Integer> move_fatal = new ArrayList<>();
+    final ArrayList<Integer> decade = new ArrayList<>();
 
     public void readCSV(InputStream csv) {
         Scanner scanner = new Scanner(csv);
@@ -31,8 +31,8 @@ public class TrainData {
     }
 
     public void calcDecade() {
-        for (Integer integer : year) {
-            decade.add((integer - 1900) / 10);
+        for (Integer currentYear : year) {
+            decade.add((currentYear - 1900) / 10);
         }
     }
 }
